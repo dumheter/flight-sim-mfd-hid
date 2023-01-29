61b5d58f0bf9f1986549c4a0f5df92e3a2ed7ce7
Christoffer Gustafsson <christoffer.golf@gmail.com>
2023-01-29 17:07:43 +0100

    Re-write the code to fit a pullup resistor instead of pulldown.

--- arduino/mfd/mfd.ino
+++ arduino/mfd/mfd.ino
@@ -1,36 +1,50 @@
 #include <Joystick.h>
 
 using Pin = unsigned int;
-constexpr Pin cathode_size = 4;
-constexpr Pin cathode_pins[cathode_size] = {2, 3, 4, 5};
-constexpr Pin anode_size = 5;
-constexpr Pin anode_pins[anode_size] = {6, 7, 8, 9, 10};
+
+/// Sink pins will do the digitalRead.
+constexpr Pin sink_size = 4;
+constexpr Pin sink_pins[sink_size] = {2, 3, 4, 5};
+
+/// Source pins will do the digitalWrite.
+constexpr Pin source_size = 5;
+constexpr Pin source_pins[source_size] = {6, 7, 8, 9, 10};
 
 Joystick_ joystick;
 
 void setup() {
-  for (Pin i = 0; i < cathode_size; ++i) {
-    pinMode(cathode_pins[i], INPUT);
+  for (Pin i = 0; i < sink_size; ++i) {
+    pinMode(sink_pins[i], INPUT_PULLUP);
   }
 
-  for (Pin i = 0; i < anode_size; ++i) {
-    pinMode(anode_pins[i], OUTPUT);
+  for (Pin i = 0; i < source_size; ++i) {
+    pinMode(source_pins[i], OUTPUT);
   }
 
   joystick.begin(false /*autosend*/);
 }
 
 void loop() {
-  for (Pin i = 0; i < anode_size; ++i) {
-    digitalWrite(anode_pins[i], HIGH);
-    delayMicroseconds(200); // debounce
+  for (Pin i = 0; i < source_size; ++i) {
+    digitalWrite(source_pins[i], LOW);
 
-    for (Pin j = 0; j < cathode_size; ++j) {
-      const auto state = digitalRead(cathode_pins[j]);
-      joystick.setButton((i) + (j) * anode_size, state);
+    // Debounce the input by reading once, then a bit later, read again.
+    // Only if the result is LOW both times should we turn on the button.
+    bool states[sink_size];
+    for (Pin j = 0; j < sink_size; ++j) {
+      states[j] = digitalRead(sink_pins[j]);
     }
 
-    digitalWrite(anode_pins[i], LOW);
+    delayMicroseconds(200);
+
+    for (Pin j = 0; j < sink_size; ++j) {
+      const auto state = states[j] || digitalRead(sink_pins[j]);
+
+      // We invert the state since we use a pullup, which natrual state is TRUE.
+      joystick.setButton((i) + (j) * source_size, !state);
+    }
+
+    digitalWrite(source_pins[i], HIGH);
   }
 
   joystick.sendState();
